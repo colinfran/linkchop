fd386e9c63a74fc32b5f0ad5f2fc3d69451fbad5
Colin Franceschini <colin.franceschini@gmail.com>
2024-03-05 11:06:33 -0800

    Update layout.tsx

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -33,8 +33,8 @@ const SettingsLayout: React.FC<Props> = ({ children }: Props) => {
   return (
     <>
       <TopNavigationAuth />
-      <div className="md:h-[calc(100vh-50px)] w-full space-y-6 py-6 xl:space-y-16">
-        <div className="container space-y-2 p-12 md:p-32">
+      <div className="w-full space-y-6 py-6 md:h-[calc(100vh-50px)] xl:space-y-16">
+        <div className="container space-y-2 p-8 md:p-32">
           <div className="size-full space-y-6 pt-10 md:block md:p-10">
             <div className="space-y-0.5">
               <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
@@ -44,7 +44,7 @@ const SettingsLayout: React.FC<Props> = ({ children }: Props) => {
             </div>
             <Separator className="my-6" />
             <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-              <aside className="-mx-4 lg:w-1/5">
+              <aside className="md:-mx-4 lg:w-1/5">
                 <SettingsNavigation items={sidebarNavItems} />
               </aside>
               <div className="flex-1 lg:max-w-2xl">{children}</div>
