8b3b6a0284a46c812ab225092c811fca11752ba4
Colin Franceschini <colin.franceschini@gmail.com>
2024-03-05 19:16:26 -0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,22 @@
 import NextAuth from 'next-auth';
 import { authConfig } from '@/app/auth/auth.config';
+import { NextResponse } from 'next/server'
+
+import type { NextRequest } from 'next/server'
 
 export default NextAuth(authConfig).auth;
+
+
+export const middleware = (req: NextRequest) => {
+  const url = req.nextUrl
+  const { pathname } = url
+  if (pathname.startsWith(`/api/`)) {
+    if (!req.headers.get("referer")?.includes(process.env.APP_URL as string)) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+  }
+  return NextResponse.next()
+}
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
