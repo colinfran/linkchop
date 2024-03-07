95f25686e7af805f0c2a9e0a12170d618ef2920a
Colin Franceschini <colin.franceschini@gmail.com>
2024-03-06 16:37:36 -0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,10 +5,16 @@ const Footer: React.FC = () => {
   return (
     <footer className="bottom-0 w-full shrink-0 border-t border-gray-200 bg-gray-200 dark:border-gray-800 dark:bg-gray-600">
       <div className="container flex flex-row justify-between gap-2 p-4 md:gap-4 md:px-6 lg:gap-6">
-        <div className="flex items-center space-x-2 text-sm text-gray-500 dark:text-gray-300">
-          <span>© 2023 LinkChop. All rights reserved.</span>
+        <div className="flex items-center space-x-2 sm:text-[12px] text-[10px] md:text-sm text-gray-500 dark:text-gray-300">
+          <span>© 2023 LinkChop</span>
         </div>
-        <nav className="flex items-center space-x-4 bg-gray-200 text-sm dark:bg-gray-600 ">
+        <nav className="flex items-center space-x-4 bg-gray-200 sm:text-[12px] text-[10px] md:text-sm dark:bg-gray-600 ">
+          <a
+            className="font-medium text-gray-900 hover:text-gray-900 dark:text-gray-50 dark:hover:text-gray-50" 
+            href="https://status.linkchop.com"
+          >
+            Status
+          </a>
           <Link
             className="font-medium text-gray-900 hover:text-gray-900 dark:text-gray-50 dark:hover:text-gray-50"
             href="/terms"
