694f18961204bffc42faf0878163d3fcf8ffb892
Colin Franceschini <hello@colinfran.com>
2024-08-23 18:18:47 -0700

    Update delete-urls.ts

--- db/tasks/delete-urls.ts
+++ db/tasks/delete-urls.ts
@@ -1,15 +1,17 @@
 import { eq } from "drizzle-orm"
 import { db } from "../init"
-import { urls } from "../tables"
+import { urls, visits } from "../tables"
 
 /**
  * Deletes a URL from the database.
+ * It also deletes any click visit data associated with that url
  * @param {string} id - The ID of the URL to be deleted.
  * @returns {Promise<boolean>} A promise that resolves a boolean if the URL deleted.
  */
 
 export const deleteUrl = async (id: string): Promise<boolean> => {
   try {
+    await db.delete(visits).where(eq(visits.url_id, id))
     await db.delete(urls).where(eq(urls.id, id))
     return true
   } catch (error) {
